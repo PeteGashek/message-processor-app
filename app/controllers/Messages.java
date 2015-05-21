7feafcade01d9292b5f02b64a3d8247734adfe9d
Zsolt Török <zsolt@zsolttorok.com>
2015-05-21 21:59:22 +0200

    Send received JSON message immediately to Websocket supervisor actor.

--- app/controllers/Messages.java
+++ app/controllers/Messages.java
@@ -2,19 +2,23 @@ package controllers;
 
 import java.util.List;
 
+import models.Constants;
 import models.MessagePublisher;
 import models.MessageRepository;
 import models.RepositoryException;
+import play.libs.Akka;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 import redis.clients.jedis.JedisPool;
+import akka.actor.ActorRef;
+import akka.actor.ActorSelection;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.typesafe.plugin.RedisPlugin;
 
 /**
- * Handles REST requests related to messages.
+ * Controller for handling REST requests related to messages.
  * 
  * @author zstorok
  */
@@ -25,10 +29,15 @@ public class Messages extends Controller {
 		if (message == null) {
 			return badRequest("Expecting application/json request body.");
 		}
+		// publish on Redis channel
 		JedisPool pool = play.Play.application().plugin(RedisPlugin.class).jedisPool();
 		MessagePublisher messagePublisher = new MessagePublisher(pool);
 		messagePublisher.publish(message);
-		// TODO: publish over Websockets as well
+		
+		// send message to connected Websocket clients via the supervisor actor
+		ActorSelection supervisor = Akka.system().actorSelection(Constants.SUPERVISOR_ACTOR_PATH);
+		supervisor.tell(message, ActorRef.noSender());
+		
 		return created();
 	}
 	
