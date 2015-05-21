fa9396688e8a6d1d89986ac12b5739b4db1c408f
Zsolt Török <zsolt@zsolttorok.com>
2015-05-21 22:02:00 +0200

    Extracted supervisor actor name and path constants.

--- app/models/Constants.java
+++ app/models/Constants.java
@@ -1,13 +1,15 @@
 package models;
 
 /**
- * Collection of constants.
+ * Collection of shared constants.
  * 
  * @author zstorok
  */
 public final class Constants {
 
 	public static final String CHANNEL_NAME = "messages";
+	public static final String SUPERVISOR_ACTOR_NAME = "supervisor";
+	public static final String SUPERVISOR_ACTOR_PATH = "/user/" + SUPERVISOR_ACTOR_NAME;
 	
 	private Constants() {}
 }
